863c7d91215fc8017e61b3788267eb220bc30f33
komeno <tkomeno@student.42tokyo.jp>
2023-12-13 22:42:29 +0900

    Updated the project up to lecture 2

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,20 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis: [String] = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚐", "🚚", "🚛", "🚜", "🚲", "🛴", "🛵", "🏍️", "🛺", "🚔", "🚍", "🚘", "🚖", "🚡", "🚠", "🚟", "🚃", "🚞", "🚝", "🚄", "🚅"]
+    
+    @State var emojiCount = 24
+    
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                add
+                Spacer()
+                remove
+            }
+            .font(.largeTitle)
         }
         .padding()
-        .foregroundColor(.red)
+    }
+    
+    var add: some View {
+        Button {
+            if (emojiCount < emojis.count) {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+    var remove: some View {
+        Button {
+            if (emojiCount > 1) {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
     }
 }
 
 struct CardView: View {
     @State var isFaceUp = true
+    var content: String
     
     var body: some View {
         ZStack {
@@ -31,8 +65,8 @@ struct CardView: View {
                     .fill()
                     .foregroundColor(.white)
                 rectangle
-                    .stroke(lineWidth: 3)
-                Text("👻")
+                    .strokeBorder(lineWidth: 3)
+                Text(content)
                     .font(.largeTitle)
             } else {
                 rectangle
